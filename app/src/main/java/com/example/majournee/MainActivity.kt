04ba3fea3130d694994bcf87f223c3727e757bae
Christian Meazza <amatim7@192.168.0.7>
2019-08-23 16:49:50 -0500

    Initial commit

---
+++ app/src/main/java/com/example/majournee/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.majournee
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+        setNavDrawer()
+    }
+
+    private fun setNavDrawer() {
+        val toggle = ActionBarDrawerToggle(this, drawer_layout, toolbar, R.string.open_drawer, R.string.closed_drawer)
+        toggle.isDrawerIndicatorEnabled = true
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+        nav_view.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        return true
+    }
+
+}
