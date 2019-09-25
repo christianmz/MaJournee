6429fdd4e7486fc8fe6e17b6321cc8aa667b0e5e
Christian Meazza <christianmeazza@gmail.com>
2019-09-25 17:58:23 -0500

    Download XML File

--- app/src/main/java/com/example/majournee/fragments/MusicFragment.kt
+++ app/src/main/java/com/example/majournee/fragments/MusicFragment.kt
@@ -1,11 +1,19 @@
 package com.example.majournee.fragments
 
+import android.os.AsyncTask
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.majournee.R
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.MalformedURLException
+import java.net.URL
 
 class MusicFragment : Fragment() {
 
@@ -14,8 +22,55 @@ class MusicFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val rootView = inflater.inflate(R.layout.fragment_music, container, false)
+
         activity?.setTitle(R.string.top_music)
-        return inflater.inflate(R.layout.fragment_music, container, false)
+        DownloaderTask().execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topsongs/limit=10/xml")
+
+        return rootView
     }
 
+    private class DownloaderTask : AsyncTask<String, Unit, String>() {
+
+        override fun doInBackground(vararg string: String): String? {
+            val rssFeed: String? = downloadXMLFile(string[0])
+            if (rssFeed == null) Log.e("MyError", "RSS NULL")
+            return rssFeed
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            Log.d("MyResult", "Apple Data: $result")
+        }
+
+        private fun downloadXMLFile(urlPath: String): String? {
+
+            val xmlResult = StringBuilder()
+
+            try {
+                val url = URL(urlPath)
+                val connection = url.openConnection() as HttpURLConnection
+                val reader = BufferedReader(InputStreamReader(connection.inputStream))
+                var charsRead: Int
+                val inputBuffer = CharArray(500)
+
+                while (true) {
+                    charsRead = reader.read(inputBuffer)
+                    if (charsRead < 0) break
+                    if (charsRead > 0) xmlResult.append(String(inputBuffer, 0, charsRead))
+                }
+                reader.close()
+                return xmlResult.toString()
+
+
+            } catch (e: MalformedURLException) {
+                Log.e("MyError", "URL Error")
+            }
+
+            catch (e: IOException) {
+                Log.e("MyError", "IOException Error")
+            }
+            return null
+        }
+    }
 }
---
+++ app/src/main/java/com/example/majournee/models/Song.kt
@@ -0,0 +1,3 @@
+package com.example.majournee.models
+
+data class Song(val name: String, val artist: String, val image: String)
